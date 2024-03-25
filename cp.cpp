69a83bd0c6f10c852d1a71b4de4d33e0b7671301
Singh, Dhiresh X <dhiresh.x.singh@questdiagnostics.com>
2024-03-25 17:09:50 +0530

    feat: added divide function

--- cp.cpp
+++ cp.cpp
@@ -1,6 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int divide(int a, int b)
+{
+    if (b != 0)
+        return a / b;
+    cout << "divide by zero error!\n";
+}
+
 int subtract(int a, int b)
 {
     int c = a - b;
